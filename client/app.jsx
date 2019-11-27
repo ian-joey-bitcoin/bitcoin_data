f2f1279c1d14026bc012f965e732185fa940c2ab
eeeeman <eeeeman@icloud.com>
2019-11-26 19:18:15 -0500

    reduced to one componenet

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Canvas from "./canvas.jsx";
+import Chart from "chart.js";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.chartRef = React.createRef();
   }
 
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+
+    new Chart(myChartRef, {
+      type: "line",
+      data: {
+        //Bring in data
+        labels: ["Jan", "Feb", "March", "newthing"],
+        datasets: [
+          {
+            label: "Sales",
+            data: [86, 67, 91]
+          }
+        ]
+      },
+      options: {
+        //Customize chart options
+      }
+    });
+  }
   render() {
+    let string = "<div>hi</div>";
+
     return (
       <div>
-        <Canvas />
+        <canvas id="myChart" ref={this.chartRef} />
       </div>
     );
   }
